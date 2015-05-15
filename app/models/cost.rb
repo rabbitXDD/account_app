d5b7fba75e6f98f4b6087d8af00ef66f277e7e8b
David <rockmanx41024@yahoo.com.tw>
2015-05-16 00:24:58 +0800

    debug highchart

--- app/models/cost.rb
+++ app/models/cost.rb
@@ -1,6 +1,7 @@
 class Cost < ActiveRecord::Base
 	belongs_to :user
 
+
 	def self.dashboard(current_user)
 		data=Cost.select("category, sum(money) as total").where({ user: current_user }).group("category")
 		data.map{|d|[d.category,d.total]}
